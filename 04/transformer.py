814fa437f5340be47d0c6808669ef3d2071971fa
sky2idx <451481604@qq.com>
2025-08-31 14:28:06 +0800

    add token generate

--- 04/transformer.py
+++ 04/transformer.py
@@ -124,6 +124,17 @@ class DumpGPTModel(torch.nn.Module):
         #output layer
         logits = self.out_head(normed_output)
         return logits
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range (max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=-1)
+    return idx
+
 
 if __name__ == "__main__":
     GPT_CONFIG_124M = {
